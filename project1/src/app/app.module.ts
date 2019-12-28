aeef578af3519f4fbab2fc0361894af66f6f2f60
Taylor Maxfield <maxfieldtaylor24@yahoo.com>
2019-12-27 16:09:43 -0800

    base angular app

---
+++ project1/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { FormsModule } from '@angular/forms';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+// Personal Imports
+import { NgbdCarouselPause } from './components/carousel-pause';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NgbdCarouselPause,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    NgbModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }

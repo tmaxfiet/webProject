ec60c1f1a822d8b2f715792f1d0f90f4fbdc134e
Taylor Maxfield <maxfieldtaylor24@yahoo.com>
2020-07-09 17:58:23 -0700

    domain with https

--- react-app/src/Settings/constants.js
+++ react-app/src/Settings/constants.js
@@ -1,4 +1,4 @@
 export const collectionNamesRoute = 'data/collectionNames';
 
 //  window.location.protocol + "//" + window.location.hostname
-export const dbBaseUrl = 'http://ec2-100-25-151-19.compute-1.amazonaws.com:3001';
+export const dbBaseUrl = 'https://study-noise.taylor-maxfield-server.com';
